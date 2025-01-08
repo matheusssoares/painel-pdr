71c2cf406c19e3745327ad917e97d76116ddaf3b
matheusssoares <matheusssoares04@gmail.com>
2025-01-08 15:29:07 -0300

    fix: corrigindo bug de rotas ativas no menu

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -13,11 +13,19 @@ import { B4aServiceService } from '../../services/b4a-service.service';
 export class MenuComponent implements OnInit {
   private b4aService = inject(B4aServiceService);
   items: NbMenuItem[] = [];	
+
+  constructor(
+  ) { }
+
   ngOnInit(): void {
     this.b4aService.getMenu().subscribe((res: any) => {
       if (res) {
-        // Mapear os dados para o formato esperado por NbMenu
-       this.items = res.result.data;
+        this.items = res.result.data.map((item: any) => ({
+          title: item.title,
+          icon: item.icon,
+          link: item.url, // 'url' da API vira 'link' para o NbMenu
+          home: item.order === 0 // Define 'home' como true para o primeiro item
+        }));
       } else {
         console.error("Erro ao carregar o menu:", res.message);
       }
