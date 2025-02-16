b58e795a3375be84f5b49a5632a35f28e1c43c17
matheusssoares <matheusssoares04@gmail.com>
2025-02-16 09:14:38 -0300

    feat: initial page reports

--- src/app/pages/admin/reports/reports.component.ts
+++ src/app/pages/admin/reports/reports.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { PrimeNgModule } from '../../../modules/primeng.module';
+import { SharedModule } from '../../../modules/shared.module';
 
 @Component({
   selector: 'app-reports',
-  imports: [],
+  imports: [SharedModule, PrimeNgModule],
   templateUrl: './reports.component.html',
   styleUrl: './reports.component.scss'
 })
